89119b5f92bad249429fef93d3b78af59048971d
“kauhik” <kaushikmanian456@gmail.com>
2025-10-19 13:35:49 +0800

    Initial Commit

---
+++ AccessibilityTest/AccessibilityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTestApp.swift
+//  AccessibilityTest
+//
+//  Created by Kaushik Manian on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilityTestTests/AccessibilityTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTestTests.swift
+//  AccessibilityTestTests
+//
+//  Created by Kaushik Manian on 19/10/25.
+//
+
+import Testing
+@testable import AccessibilityTest
+
+struct AccessibilityTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
